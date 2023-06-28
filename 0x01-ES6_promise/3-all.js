2b4a540e6f9462964990b58e9bbd47fe2e462c23
MikeRock51 <mikerockmusic51@gmail.com>
2023-06-28 12:15:51 +0100

    Use template literals

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photo, userInfo]) => {
-      console.log(photo.body, userInfo.firstName, userInfo.lastName);
+      console.log(`${photo.body} ${userInfo.firstName} ${userInfo.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
