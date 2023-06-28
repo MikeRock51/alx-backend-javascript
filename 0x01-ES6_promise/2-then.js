ca26d1ad7835e0e39f24564a3a3cac7afe25eee0
MikeRock51 <mikerockmusic51@gmail.com>
2023-06-28 10:36:33 +0100

    Update 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 function handleResponseFromAPI(promise) {
   promise
-    .then(() => ({ status: 200, body: 'success' })).then(() => {
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error()).finally(() => {
       console.log('Got a response from the API');
-    })
-    .catch(() => Error());
+    });
 }
 
 export default handleResponseFromAPI;
