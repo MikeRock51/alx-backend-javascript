2beb63903378c905c8c8e126e751793ee704dd96
MikeRock51 <mikerockmusic51@gmail.com>
2023-06-28 11:55:51 +0100

    Update t3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,22 +1,22 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  const photo = uploadPhoto();
-  const user = createUser();
   let body;
   let firstName;
   let lastName;
 
-  photo.then((response) => {
-    ({ body } = response);
-    user.then((response) => {
-      ({ firstName, lastName } = response);
+  uploadPhoto().then((photo) => {
+    ({ body } = photo);
+    return createUser();
+  })
+    .then((userInfo) => {
+      ({ firstName, lastName } = userInfo);
+    }).then(() => {
+      console.log(body, firstName, lastName);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
     });
-  }).then(() => {
-    console.log(body, firstName, lastName);
-  }).catch(() => {
-    console.log('Signup system offline');
-  });
 }
 
 export default handleProfileSignup;
