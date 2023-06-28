84f974b52463122fbb08d6d945b1a47909543b15
MikeRock51 <mikerockmusic51@gmail.com>
2023-06-28 15:15:17 +0100

    Add Task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,25 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+function handleProfileSignUp(firstName, lastName, fileName) {
+  const outcome = [];
+
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          outcome.push({
+            status: result.status,
+            value: result.value,
+          });
+        } else {
+          outcome.push({
+            status: result.status,
+            value: result.reason,
+          });
+        }
+      });
+    }).then(() => outcome);
+}
+
+export default handleProfileSignUp;
