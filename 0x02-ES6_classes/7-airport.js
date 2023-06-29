f9dc6e03525af689b085ff0154ae2ad29e8eb68e
MikeRock51 <mikerockmusic51@gmail.com>
2023-06-29 16:24:36 +0100

    Add Task 7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,5 +1,19 @@
+/* eslint-disable no-underscore-dangle */
 class Airport {
-  constructor()
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return this._code;
+  }
 }
 
 export default Airport;
