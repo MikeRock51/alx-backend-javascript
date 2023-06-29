4d5bf7b997b201eca120af465fa66f5c98f2c7c7
MikeRock51 <mikerockmusic51@gmail.com>
2023-06-29 15:49:56 +0100

    Add Task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-underscore-dangle */
+class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    if (typeof sqft !== 'number') {
+      throw TypeError('sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(val) {
+    if (typeof val !== 'number') {
+      throw TypeError('sqft must be a number');
+    }
+    this._sqft = val;
+  }
+}
+
+export default Building;
