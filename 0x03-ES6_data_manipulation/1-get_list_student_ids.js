f34060a4ce443e5418d882ff2c93d40eaa1a43d2
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-03 11:43:49 +0100

    Add Task 1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+function getListStudentIds(studentsList) {
+  if (!Array.isArray(studentsList)) {
+    return [];
+  }
+  return studentsList.map((student) => student.id);
+}
+
+export default getListStudentIds;
