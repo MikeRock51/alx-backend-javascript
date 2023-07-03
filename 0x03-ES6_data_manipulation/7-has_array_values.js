7d3e4d22b604f1a6142fb3fc54e7be07900777f2
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-03 14:14:40 +0100

    Add Task 7

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,10 @@
+function hasValuesFromArray(set, array) {
+  for (const val of array) {
+    if (!set.has(val)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export default hasValuesFromArray;
