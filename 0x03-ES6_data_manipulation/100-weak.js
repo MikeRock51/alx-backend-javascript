1074f231ba4d59a60e1dbf09197e032cc38603f3
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-03 17:29:16 +0100

    Add Task 11

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,9 @@
+export const weakMap = new WeakMap();
+export function queryAPI(endpoint) {
+  const calls = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, calls + 1);
+
+  if (calls >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
