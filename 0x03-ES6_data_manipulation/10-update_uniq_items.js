f06e7c1bff18ed4e35518c286ca7851e3d749793
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-03 16:05:00 +0100

    Add Task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,10 @@
 function updateUniqueItems(items) {
-  
+  items.forEach((item, key) => {
+    if (item === 1) {
+      items.set(key, 100);
+    }
+  });
+  return items;
 }
 
 export default updateUniqueItems;
