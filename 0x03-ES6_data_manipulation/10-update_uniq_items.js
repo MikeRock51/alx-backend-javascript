76180f25ccb47586cb372430a49c55f1045e67a3
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-04 11:50:50 +0100

    Add type check for task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,8 @@
 function updateUniqueItems(items) {
+  if (items instanceof Map !== true) {
+    throw new Error('Cannot process');
+  }
+
   items.forEach((item, key) => {
     if (item === 1) {
       items.set(key, 100);
