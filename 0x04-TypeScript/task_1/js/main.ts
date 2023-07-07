0d0e9c699159f5b1dacb1687b2455af99ec7b195
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-07 10:25:26 +0100

    Add Task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,4 +19,30 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName[0]}. ${lastName}`;
 }
 
-// console.log(printTeacher("Mercy", "Godwin"));
+interface studentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+
+class StudentClass implements studentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+// const student = new StudentClass("Mike", "Rock");
+// console.log(student.displayName());
+// console.log(student.workOnHomework());
