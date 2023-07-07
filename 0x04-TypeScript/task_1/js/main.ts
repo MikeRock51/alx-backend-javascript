c6359c8af8a40fde5e1af7514bec526a5f043356
MikeRock51 <mikerockmusic51@gmail.com>
2023-07-07 09:24:29 +0100

    Add task 2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,12 +7,15 @@ interface Teacher {
     [key: string]: any;
 }
 
-// const teacher3: Teacher = {
-//     firstName: 'John',
-//     fullTimeEmployee: false,
-//     lastName: 'Doe',
-//     location: 'London',
-//     contract: false,
-//   };
+interface Director extends Teacher {
+    numberOfReports: number;
+}
 
-//   console.log(teacher3);
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+console.log(director1);
