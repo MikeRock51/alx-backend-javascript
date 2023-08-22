c066e011d24bbbe113be699dda03ea78ac18684f
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-22 07:28:54 +0100

    Write with process

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,10 +2,10 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const nam = process.stdin.read();
   if (nam !== null) {
-    console.log(`Your name is: ${nam.toString().trim()}`);
+    process.stdout.write(`Your name is: ${nam.toString().trim()}\n`);
   }
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
