252e61927bbfd99b2c65dcf84c9162e8e9d33053
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-22 23:01:48 +0100

    Add Task 8

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -2,27 +2,36 @@ import readDatabase from '../utils';
 
 class StudentsController {
   static getAllStudents(request, response) {
-    res = 'This is the list of our students\n';
-    readDatabase(process.argv[2]).then((students) => {
-      for (const entry of Object.entries(students).sort()) {
-        response.status(200).send(`${res}Number of students in ${entry}: ${students[entry[0]].length}. List: ${students[entry[1]].join(', ')}`);
-      }
-      return response.status(200);
-    }).catch((err) => {
-      return response.status(500).send(err.message);
-    });
+    let res = 'This is the list of our students\n';
+    readDatabase(process.argv[2])
+      .then((students) => {
+        for (const entry of Object.entries(students).sort()) {
+          res += `Number of students in ${entry[0]}: ${
+            entry[1].length
+          }.\nList: ${entry[1].join(', ')}\n`;
+        }
+        response.send(res.trim());
+        return response.status(200);
+      })
+      .catch((err) => response.status(500).send(err.message));
   }
 
+  // eslint-disable-next-line consistent-return
   static getAllStudentsByMajor(request, response) {
-    const { major } = request.params();
+    const { major } = request.params;
     if (major !== 'CS' && major !== 'SWE') {
-      return response.status(500).send('Major parameter must be CS or SWE');
-    readDatabase(process.argv[2]).then((students) => {
-      response.status(200).send(`List: ${students[major]}`);
-    }).catch((err) => {
-      response.status(500).send(err.message);
-    });
+      response.send('Major parameter must be CS or SWE');
+      return response.status(500);
     }
+    readDatabase(process.argv[2])
+      .then((students) => {
+        response.send(`List: ${students[major].join(', ')}`);
+        return response.status(200);
+      })
+      .catch((err) => {
+        response.send(err.message);
+        return response.status(500);
+      });
   }
 }
 
