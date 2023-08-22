6bb5e50fb6968e63bad2146ddee7725d3f844a36
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-22 07:35:11 +0100

    Add missing ?

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-process.stdout.write('Welcome to Holberton School, what is your name\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const nam = process.stdin.read();
   if (nam !== null) {
