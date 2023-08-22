6147ec25e73f0f2b96eac3df7c6b0d748c18fb45
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-22 07:31:48 +0100

    Remove trim

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name\n');
 process.stdin.on('readable', () => {
   const nam = process.stdin.read();
   if (nam !== null) {
-    process.stdout.write(`Your name is: ${nam.toString().trim()}\n`);
+    process.stdout.write(`Your name is: ${nam.toString()}`);
   }
 });
 
