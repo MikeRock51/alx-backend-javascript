767b9bac3406442112bbe85996030cf1ba16f193
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-22 14:26:07 +0100

    Add Task 7

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    const students = {};
+
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+      } else {
+        const lines = data.split('\n');
+        if (lines[lines.length - 1] === '') {
+          lines.pop();
+        }
+        const numStudents = lines.length - 1;
+
+        for (let i = 1; i < lines.length; i += 1) {
+          const student = lines[i].split(',');
+          const field = student[3];
+          const firstname = student[0];
+
+          if (students[field]) {
+            students[field].push(firstname);
+          } else {
+            students[field] = [firstname];
+          }
+        }
+        let output = `Number of students: ${numStudents}\n`;
+        for (const field in students) {
+          if (students[field]) {
+            const firstnames = students[field].join(', ');
+            output += `Number of students in ${field}: ${students[field].length}. List: ${firstnames}\n`;
+          }
+        }
+        resolve(output);
+      }
+    });
+  });
+}
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2]).then((output) => {
+    const str = `This is the list of our students\n${output}`;
+    res.send(str.trim());
+  }).catch((err) => {
+    res.send(err.message);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
+module.exports = app;
