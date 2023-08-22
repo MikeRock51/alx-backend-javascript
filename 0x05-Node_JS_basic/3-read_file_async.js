b6cff95fd1e8dc005906792cfd0d34dbb84527fa
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-22 12:27:47 +0100

    Update Task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -3,37 +3,40 @@ const fs = require('fs');
 function countStudents(path) {
   return new Promise((resolve, reject) => {
     const students = {};
-    try {
-      const data = fs.readFileSync(path, 'utf-8');
-      const lines = data.split('\n');
-      if (lines[lines.length - 1] === '') {
-        lines.pop();
-      }
-      const numStudents = lines.length - 1;
 
-      for (let i = 1; i < lines.length; i += 1) {
-        const student = lines[i].split(',');
-        const field = student[3];
-        const firstname = student[0];
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+      } else {
+        const lines = data.split('\n');
+        if (lines[lines.length - 1] === '') {
+          lines.pop();
+        }
+        const numStudents = lines.length - 1;
 
-        if (students[field]) {
-          students[field].push(firstname);
-        } else {
-          students[field] = [firstname];
+        for (let i = 1; i < lines.length; i += 1) {
+          const student = lines[i].split(',');
+          const field = student[3];
+          const firstname = student[0];
+
+          if (students[field]) {
+            students[field].push(firstname);
+          } else {
+            students[field] = [firstname];
+          }
         }
-      }
-      console.log(`Number of students: ${numStudents}`);
-      for (const field in students) {
-        if (students[field]) {
-          const firstnames = students[field].join(', ');
-          console.log(
-            `Number of students in ${field}: ${students[field].length}. List: ${firstnames}`,
-          );
+        console.log(`Number of students: ${numStudents}`);
+        for (const field in students) {
+          if (students[field]) {
+            const firstnames = students[field].join(', ');
+            console.log(
+              `Number of students in ${field}: ${students[field].length}. List: ${firstnames}`,
+            );
+          }
         }
+        resolve(students);
       }
-    } catch (err) {
-      reject(Error('Cannot load the database'));
-    }
+    });
   });
 }
 
