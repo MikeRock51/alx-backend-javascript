6a0b1a792924da90a3d72f6394880dc6bc199bab
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-22 10:25:11 +0100

    Task 3...

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  const students = {};
+
+  fs.readFile(path, 'utf-8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    const lines = data.split('\n');
+    if (lines[lines.length - 1] === '') {
+      lines.pop();
+    }
+    const numStudents = lines.length - 1;
+
+    for (let i = 1; i < lines.length; i += 1) {
+      const student = lines[i].split(',');
+      const field = student[3];
+      const firstname = student[0];
+
+      if (students[field]) {
+        students[field].push(firstname);
+      } else {
+        students[field] = [firstname];
+      }
+    }
+    console.log(`Number of students: ${numStudents}`);
+    for (const field in students) {
+      if (students[field]) {
+        const firstnames = students[field].join(', ');
+        console.log(
+          `Number of students in ${field}: ${students[field].length}. List: ${firstnames}`,
+        );
+      }
+    }
+  });
+}
+
+module.exports = countStudents;
