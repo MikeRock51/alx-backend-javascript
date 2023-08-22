3af4be740ca19a7bbefaec919540103164d9dfb4
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-22 07:33:19 +0100

    Remove toString

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name\n');
 process.stdin.on('readable', () => {
   const nam = process.stdin.read();
   if (nam !== null) {
-    process.stdout.write(`Your name is: ${nam.toString()}`);
+    process.stdout.write(`Your name is: ${nam}`);
   }
 });
 
