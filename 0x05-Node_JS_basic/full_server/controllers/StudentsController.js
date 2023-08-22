6a0cac0ac0eabd235c5928d47d3719b7082cbc43
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-22 20:12:19 +0100

    Task 8... Low battery

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -2,20 +2,27 @@ import readDatabase from '../utils';
 
 class StudentsController {
   static getAllStudents(request, response) {
-    console.log('This is the list of our students');
+    res = 'This is the list of our students\n';
     readDatabase(process.argv[2]).then((students) => {
       for (const entry of Object.entries(students).sort()) {
-        console.log(`Number of students in ${entry}: ${students[entry[0]].length}. List: ${students[entry[1]].join(', ')}`);
+        response.status(200).send(`${res}Number of students in ${entry}: ${students[entry[0]].length}. List: ${students[entry[1]].join(', ')}`);
       }
       return response.status(200);
     }).catch((err) => {
-      console.log(err.message);
-      return response.status(500);
+      return response.status(500).send(err.message);
     });
   }
 
   static getAllStudentsByMajor(request, response) {
-    
+    const { major } = request.params();
+    if (major !== 'CS' && major !== 'SWE') {
+      return response.status(500).send('Major parameter must be CS or SWE');
+    readDatabase(process.argv[2]).then((students) => {
+      response.status(200).send(`List: ${students[major]}`);
+    }).catch((err) => {
+      response.status(500).send(err.message);
+    });
+    }
   }
 }
 
