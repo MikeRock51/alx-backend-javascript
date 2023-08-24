3667007923c5a2c11993629b70e9ed5ef890fbfc
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-24 19:58:51 +0100

    On 10 Low battery

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require('body-parser')
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+const port = 7865;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the payment system");
+});
+
+app.get("/cart/:id(\\d+)", (req, res) => {
+  const id = req.params.id;
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.get("/available_payments", (req, res) => {
+  res.send({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  });
+});
+
+app.post('/login', (req, res) => {
+  const userName = req.body.userName;
+  res.send(`Welcome ${userName}`);
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,20 @@
+const { expect } = require("chai");
+const { response } = require("express");
+const request = require("request");
+
+describe("Integration Test", function () {
+  it("Tests the /cart route with number", function (done) {
+    request("http://127.0.0.1:7865/cart/18", function (error, response, body) {
+      expect(response.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 18');
+      done();
+    });
+  });
+
+  it("Tests the /cart route with NaN", function (done) {
+    request("http://127.0.0.1:7865/cart/mike", function (error, response, body) {
+      expect(response.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+});
