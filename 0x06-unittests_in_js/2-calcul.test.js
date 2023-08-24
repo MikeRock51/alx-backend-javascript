d23c46d8d33ab11296000caa5adaa492d78c05b2
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-24 06:17:55 +0100

    Add Task 2

--- 0x06-unittests_in_js/2-calcul.test.js
+++ 0x06-unittests_in_js/2-calcul.test.js
@@ -7,52 +7,52 @@ describe("Test calculateNumber", function () {
       expect(calculateNumber("SUM", 2.8, 3.2)).to.equal(6);
     });
     it("Checks if numbers round", function () {
-      assert.notEqual(calculateNumber("SUM", 2.8, 3.1), 5.9);
+      expect(calculateNumber("SUM", 2.8, 3.1)).to.not.be.equal(5.9);
     });
     it("Checks if negative numbers round", function () {
-      assert.equal(calculateNumber("SUM", -10.5, -69.2), -79);
+      expect(calculateNumber("SUM", -10.5, -69.2)).to.be.equal(-79);
     });
     it("Checks negative arg 1 round", function () {
-      assert.equal(calculateNumber("SUM", -10.5, 69.2), 59);
+      expect(calculateNumber("SUM", -10.5, 69.2)).to.be.equal(59);
     });
     it("Checks negative arg 2 round", function () {
-      assert.equal(calculateNumber("SUM", 10.5, -69.2), -58);
+      expect(calculateNumber("SUM", 10.5, -69.2)).to.be.equal(-58);
     });
   });
 
   describe("Test SUBTRACT", function () {
     it("Checks for equality", function () {
-      assert.equal(calculateNumber("SUBTRACT", 2.8, 3.2), 0);
+      expect(calculateNumber("SUBTRACT", 2.8, 3.2)).to.be.equal(0);
     });
     it("Checks if numbers round", function () {
-      assert.notEqual(calculateNumber("SUBTRACT", 5.5, 2.0), 4.5);
+      expect(calculateNumber("SUBTRACT", 5.5, 2.0)).to.not.be.equal(4.5);
     });
     it("Checks if negative numbers round", function () {
-      assert.equal(calculateNumber("SUBTRACT", -69.2, -10.5), -59);
+      expect(calculateNumber("SUBTRACT", -69.2, -10.5)).to.be.equal(-59);
     });
     it("Checks negative arg 1 round", function () {
-      assert.equal(calculateNumber("SUBTRACT", -10.5, 69.2), -79);
+      expect(calculateNumber("SUBTRACT", -10.5, 69.2)).to.be.equal(-79);
     });
     it("Checks negative arg 2 round", function () {
-      assert.equal(calculateNumber("SUBTRACT", 80.3, -6.2), 86);
+      expect(calculateNumber("SUBTRACT", 80.3, -6.2)).to.be.equal(86);
     });
   });
 
   describe("Test DIVIDE", function () {
     it("Checks for equality", function () {
-      assert.equal(calculateNumber("DIVIDE", 2.8, 3.2), 1);
+      expect(calculateNumber("DIVIDE", 2.8, 3.2)).to.be.equal(1);
     });
     it("Checks if numbers round", function () {
-      assert.notEqual(calculateNumber("DIVIDE", 5.5, 2.0), 3.5);
+      expect(calculateNumber("DIVIDE", 5.5, 2.0)).to.not.be.equal(3.5);
     });
     it("Checks if negative numbers round", function () {
-      assert.equal(calculateNumber("DIVIDE", -69.2, -10.5), 6.9);
+      expect(calculateNumber("DIVIDE", -69.2, -10.5)).to.be.equal(6.9);
     });
     it("Checks divide by 0", function () {
-      assert.equal(calculateNumber("DIVIDE", 10.5, 0), "Error");
+      expect(calculateNumber("DIVIDE", 10.5, 0)).to.be.equal("Error");
     });
     it("Checks 0 arg 1", function () {
-      assert.equal(calculateNumber("DIVIDE", 0, 4.2), 0);
+      expect(calculateNumber("DIVIDE", 0, 4.2)).to.be.equal(0);
     });
   });
 });
