a5dcee85b1021a55604d4938b7541b1dc82dcecf
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-24 16:58:14 +0100

    Add Task 6...

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,14 @@
+const sinon = require("sinon");
+const { expect } = require("chai");
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe("Test getPaymentTokenFromAPI", function () {
+  it("Runs an async test", function (done) {
+    this.timeout(0);
+    getPaymentTokenFromAPI(true).then((data) => {
+        expect(data.data).to.be.equal('Successful response from the API');
+        expect(typeof(data)).to.be.equal('object');
+        done();
+    });
+  });
+});
