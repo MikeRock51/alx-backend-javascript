1283aa1cfe1648303777d87fc81c38278093e173
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-24 17:17:58 +0100

    Update Task 6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,14 +1,21 @@
 const sinon = require("sinon");
 const { expect } = require("chai");
-const getPaymentTokenFromAPI = require('./6-payment_token');
+const getPaymentTokenFromAPI = require("./6-payment_token");
 
 describe("Test getPaymentTokenFromAPI", function () {
   it("Runs an async test", function (done) {
+    getPaymentTokenFromAPI(true).then((data) => {
+      expect(data.data).to.be.equal("Successful response from the API");
+      expect(typeof data).to.be.equal("object");
+      done();
+    });
+  });
+
+  it("Tests unresolved promise", function (done) {
     this.timeout(0);
     getPaymentTokenFromAPI(true).then((data) => {
-        expect(data.data).to.be.equal('Successful response from the API');
-        expect(typeof(data)).to.be.equal('object');
-        done();
+      expect(data).to.equal('');
+      done();
     });
   });
 });
