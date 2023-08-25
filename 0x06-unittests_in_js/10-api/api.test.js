7f23ae56d48d6eecb87dd7ee303a40c3b5e91117
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-25 12:35:36 +0100

    Add test for / in task 9

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -3,6 +3,14 @@ const { response } = require("express");
 const request = require("request");
 
 describe("Integration Test", function () {
+  it("Tests the index page", function (done) {
+    request("http://127.0.0.1:7865/", function (error, response, body) {
+      expect(response.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
   it("Tests the /cart route with number", function (done) {
     request("http://127.0.0.1:7865/cart/18", function (error, response, body) {
       expect(response.statusCode).to.be.equal(200);
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -3,6 +3,20 @@ const { response } = require("express");
 const request = require("request");
 
 describe("Integration Test", function () {
+  it("Tests the index page status code", function (done) {
+    request("http://127.0.0.1:7865/", function (error, response, body) {
+      expect(response.statusCode).to.be.equal(200);
+      done();
+    });
+  });
+
+  it("Tests the index page response message", function (done) {
+    request("http://127.0.0.1:7865/", function (error, response, body) {
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
   it("Tests the /cart route with number", function (done) {
     request("http://127.0.0.1:7865/cart/18", function (error, response, body) {
       expect(response.statusCode).to.be.equal(200);
