7bc7a1c9cf7ac6e062518b76e6b4451b4821f502
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-25 16:46:13 +0100

    Add Task 10

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,11 +1,17 @@
 const { expect } = require("chai");
-const { response } = require("express");
+// const { response } = require("express");
 const request = require("request");
 
 describe("Integration Test", function () {
-  it("Tests the index page", function (done) {
+  it("Tests the index page status code", function (done) {
     request("http://127.0.0.1:7865/", function (error, response, body) {
       expect(response.statusCode).to.be.equal(200);
+      done();
+    });
+  });
+
+  it("Tests the index page response message", function (done) {
+    request("http://127.0.0.1:7865/", function (error, response, body) {
       expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
@@ -22,6 +28,20 @@ describe("Integration Test", function () {
   it("Tests the /cart route with NaN", function (done) {
     request("http://127.0.0.1:7865/cart/mike", function (error, response, body) {
       expect(response.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it("Tests the /login route", function (done) {
+    const opt = {
+      url: "http://127.0.0.1:7865/login",
+      json: true,
+      body: {userName: "Mike Rock"},
+      method: "POST"
+    };
+    request(opt, function (error, response, body) {
+      expect(response.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome Mike Rock');
       done();
     });
   });
